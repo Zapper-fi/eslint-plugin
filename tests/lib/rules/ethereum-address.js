89e8a57099bf129e98c038744a05e367a048b90e
Justin D'Errico <justin.derrico1991@gmail.com>
2022-07-21 11:34:09 -0400

    feat(package): Update package name and add template literal

--- tests/lib/rules/ethereum-address.js
+++ tests/lib/rules/ethereum-address.js
@@ -18,31 +18,31 @@ const RuleTester = require('eslint').RuleTester;
 const ruleTester = new RuleTester();
 ruleTester.run('ethereum-address', rule, {
   valid: [
-    '0xe27bff97ce92c3e1ff7aa9f86781fdd6d48f5ee9',
-    'test(0xe27bff97ce92c3e1ff7aa9f86781fdd6d48f5ee9)',
-    'test({ address: 0xe27bff97ce92c3e1ff7aa9f86781fdd6d48f5ee9 })',
+    "'0xe27bff97ce92c3e1ff7aa9f86781fdd6d48f5ee9'",
+    "test('0xe27bff97ce92c3e1ff7aa9f86781fdd6d48f5ee9')",
+    "test({ address: '0xe27bff97ce92c3e1ff7aa9f86781fdd6d48f5ee9' })",
   ],
 
   invalid: [
     {
-      code: '0xE27BFF97CE92C3E1FF7AA9F86781FDD6D48F5EE9',
+      code: "'0xE27BFF97CE92C3E1FF7AA9F86781FDD6D48F5EE9'",
       errors: [
         {
           message: 'Avoid using Ethereum addresses with uppercase characters',
           type: 'Literal',
         },
       ],
-      output: '0xe27bff97ce92c3e1ff7aa9f86781fdd6d48f5ee9',
+      output: "'0xe27bff97ce92c3e1ff7aa9f86781fdd6d48f5ee9'",
     },
     {
-      code: 'this.test(0xAAAAFF97CE92C3E1FF7AA9F86781FDD6D48F5EE9)',
+      code: "this.test('0xAAAAFF97CE92C3E1FF7AA9F86781FDD6D48F5EE9')",
       errors: [
         {
           message: 'Avoid using Ethereum addresses with uppercase characters',
           type: 'Literal',
         },
       ],
-      output: 'this.test(0xaaaaff97ce92c3e1ff7aa9f86781fdd6d48f5ee9)',
+      output: "this.test('0xaaaaff97ce92c3e1ff7aa9f86781fdd6d48f5ee9')",
     },
   ],
 });
